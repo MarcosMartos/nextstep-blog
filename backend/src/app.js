4c9d27a18202674ab29eb41fd87a39631504b48b
Marcos <marcosmartostuvi@gmail.com>
2025-04-10 18:19:11 -0300

    configuracion de base de datos

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use("/posts", postRoutes);
